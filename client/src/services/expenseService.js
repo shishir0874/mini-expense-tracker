a330b2e4446be13db7ac70b2e06464a91aa609cf
shishir0874 <shishirsharma874@gmail.com>
2026-06-10 22:32:30 +0530

    Prepare application for deployment

--- client/src/services/expenseService.js
+++ client/src/services/expenseService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  "http://localhost:5000/api/expenses";
+  `${import.meta.env.VITE_API_URL}/expenses`;
 
 export const getExpenses = () =>
   axios.get(API_URL);
